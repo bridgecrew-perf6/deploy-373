85f7adcd1c5dc1795c09f78fa43cfdcf64f5346a
Aman Sharma <bobbysharma6593@gmail.com>
2022-02-15 18:57:23 +0530

    images replace videos

--- remind/urls.py
+++ remind/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.searchUser),
     path('admin/', admin.site.urls),
+    path('video-player', views.videoPlayer),
+    path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
